aca8e53c59232b733b872c59da83d9ba1463f352
Suuunz <ysj18805@khu.ac.kr>
2026-03-28 02:24:10 +0900

    chore : 상위 폴더 기준으로 프로젝트 구조 재구성

---
+++ khunect-backend/src/main/java/com/khunect/backend/KhunectBackendApplication.java
@@ -0,0 +1,13 @@
+package com.khunect.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KhunectBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KhunectBackendApplication.class, args);
+	}
+
+}
